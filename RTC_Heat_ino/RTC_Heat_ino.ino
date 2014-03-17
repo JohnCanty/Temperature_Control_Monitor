2c14ed3fb2dfc64a70cfcd8cae07163abbbbab2c
John Canty <JCanty@hush.com>
2014-03-16 21:48:44 -0400

    Tested heat control function

--- RTC_Heat_ino/RTC_Heat_ino.ino
+++ RTC_Heat_ino/RTC_Heat_ino.ino
@@ -16,35 +16,36 @@ A5 SQW
 #include <EthernetUdp.h>
 #include <Wire.h>
 #include <RTClib.h>
-#include <EEPROM.h>
+#include <dht11.h>
+
+
 
 //RTC Encapsulation
 RTC_DS1307 RTC;
-
+dht11 DHT11;
+#define DHT11PIN 2
 byte mac[] = { 0xDE, 0xAD, 0xFE, 0xED, 0xDE, 0xAD };//MAC Address for Ethernet Shield
-
 unsigned int localPort = 8888; //Local port to listen for UDP Packets
-
 IPAddress timeServer(192, 168, 10, 2);//NTP Server IP 
-
 const int NTP_PACKET_SIZE= 48; //NTP Time stamp is in the firth 48 bytes of the message
-
 byte packetBuffer[ NTP_PACKET_SIZE]; //Buffer to hold incomming and outgoing packets
-
 EthernetUDP Udp; //UDP Instance to let us send and recieve packets
-
 unsigned long epoch; //Unix Epoch time (NTP or RTC depending on state)
-
 int eepromaddress = 0;
-
 float temperatureDegF; //Ambient Temperature in degrees F
 int currentTemp; //Current temp in Integer form
 int temperatureSPF; //Temperature set point in Degrees F
 int temperatureDBF; //Temperature dead band in Degrees F
-byte winterTime = false; // Is it winter time
+//byte winterTime = true; // Is it winter time
 byte override = false; //override button, will turn on and off heat forcibly
 byte laststate;
 byte currentstate;
+float humidity;
+float temperature;
+float DegF;
+float kelvin;
+float DewC;
+float DewF;
 
 void setup() {
   Serial.begin(115200); //Start the serial interface
@@ -76,6 +77,7 @@ void setup() {
     Serial.println(epoch);
     //Set the RTC on startup
     RTC.adjust(epoch);
+    Serial.println("RTC Adjusted");
   //check to see if the RTC is running, display fault message
   if( !RTC.isrunning() ) {
     
@@ -93,11 +95,31 @@ void setup() {
 }
 
 void loop() {
-  
+  int chk = DHT11.read(DHT11PIN); //Fill chk with any error codes from the dht read (library Function)
+  delay(500);  
+  switch (chk) { //Do some things like print to the serial monitor if there is an error.
+    case DHTLIB_OK: // Atta Boy!
+                break;
+    case DHTLIB_ERROR_CHECKSUM:
+                Serial.println("Checksum error"); //The checksum is bad, let the user know
+                break;
+    case DHTLIB_ERROR_TIMEOUT:
+                Serial.println("Time out error"); //Is this thing connected?
+                break;
+    default:
+                Serial.println("Unknown error"); //Yeah something broke, don't ask me what it was though
+                break;
+    }
+    
   //repeatedly show the current date and time, taken from the RTC
   DateTime now = RTC.now();
-
-  Serial.print(now.unixtime());
+  DHTRead();
+  currentTemp = DegF;
+  temperatureSPF = 77;
+  temperatureDBF = 2;
+  Serial.println(now.unixtime());
+  Serial.print(" - ");
+  Serial.print(currentTemp);
   Serial.print(" - ");
   //Serial.print(now.year(), DEC);
   //Serial.print('/');
@@ -112,10 +134,11 @@ void loop() {
   //Serial.print(":");
   //Serial.println(now.second());
   //RTC.adjust(epoch);
-  pinMode(A4, OUTPUT);
+  pinMode(A0, OUTPUT);
   if (Winter(override) == true){
-  digitalWrite(A4, TempControl(currentTemp, temperatureSPF, temperatureDBF));
-  currentstate = digitalRead(A4);
+    Serial.println("wintertime");
+  digitalWrite(A0, TempControl(currentTemp, temperatureSPF, temperatureDBF));
+  currentstate = digitalRead(A0);
   if (currentstate != laststate){
     if (laststate == HIGH) {
      //Put logic in here to datalog a heat off state 
@@ -208,22 +231,37 @@ int ESTHour (){
 conception heat was the only option available to me, Expect more functionality in the future.
 */
 byte TempControl(int x, int y, int z){
- // x is ambient temperature
+// x is ambient temperature
 // y is setpoint
 // z is dead band
 int dbhi = y + z; //create integer dead band for the heat Ceiling
 int dblo = y - z; //create integer dead band for the heat Floor
-byte enable = false; //build an enable byte
+byte enableSP = false; //build an enable byte
+byte fallingEdge = false; //rising edge
 byte fire = false; //build a turn on byte
-if (x < dbhi){ //allow the enable byte to go high
- enable = true;
+byte lastState; //last state of the fire command
+byte risingEdge; //falling edge
+byte enableDB; //Dead band enable
+if (x < y){ //allow the enable byte to go high
+ enableSP = true;
 } else { //once the temperature rises to the high dead band reset everything (turn off heat)
-  enable = false;
+  enableSP = false;
   fire = false;
+  fallingEdge = false;
 }
-if (x < dblo && enable == true){ //we are enabled and we dropped below the dead band
+if (x < dblo && lastState == false){ //we dropped below the dead band and are not rising in temp
+  fallingEdge = true;
+}
+if (x > dblo && lastState == true){ //we rise above the low deadband and we are giving it juice
+  risingEdge = true;
+}
+if (fallingEdge == true || risingEdge == true){
+  enableDB = true;
+}
+if (enableDB == true && enableSP == true){
   fire = true;
 }
+lastState = fire;
 return fire;
 }
 /* The automatic function of winter time can be overridden. On warm winter days a touch of this
@@ -233,7 +271,7 @@ The override feature will be self resetting most likely with time.
 byte Winter(byte override){
   DateTime now = RTC.now();
   byte monthenable = false;
-  if (now.month() >= 11 && now.month() <= 5 ) {
+  if (now.month() >= 11 || now.month() <= 5 ) {
     monthenable = true;
   } else {
     monthenable = false;
@@ -243,5 +281,62 @@ byte Winter(byte override){
   else {
     return false;
   }
+}
+
+
+//Celsius to Fahrenheit conversion
+double Fahrenheit(double celsius) {
+        return 1.8 * celsius + 32;
+}
+
+//Celsius to Kelvin conversion
+double Kelvin(double celsius) {
+        return celsius + 273.15;
+}
+
+// delta max = 0.6544 wrt dewPoint()
+// 5x faster than dewPoint()
+// reference: http://en.wikipedia.org/wiki/Dew_point
+double dewPointFast(double celsius, double humidity) {
+        double a = 17.271;
+        double b = 237.7;
+        double temp = (a * celsius) / (b + celsius) + log(humidity/100);
+        double Td = (b * temp) / (a - temp);
+        return Td;
+}
+double dewPointFastF(double celsius, double humidity) {
+        double a = 17.271;
+        double b = 237.7;
+        double temp = (a * celsius) / (b + celsius) + log(humidity/100);
+        double Tf = (b * temp) / (a - temp);
+        return 1.8 * Tf + 32;
+}
+void DHTRead() {
+  Serial.print("Humidity (%): ");
+  humidity = DHT11.humidity;
+  Serial.println(humidity, 2);
   
+  
+  Serial.print("Temperature (oC): ");
+  temperature = DHT11.temperature;
+  Serial.println(temperature, 2);
+  
+  
+  Serial.print("Temperature (oF): ");
+  DegF = (Fahrenheit(DHT11.temperature));
+  Serial.println(DegF, 2);
+
+  Serial.print("Temperature (K): ");
+  kelvin = (Kelvin(DHT11.temperature));
+  Serial.println(kelvin , 2);
+
+  Serial.print("Dew Point (oC): ");
+  DewC = (dewPointFast(DHT11.temperature, DHT11.humidity));
+  Serial.println(DewC , 2);
+
+  Serial.print("Dew Point (oF): ");
+  DewF = (dewPointFastF(DHT11.temperature, DHT11.humidity));
+  Serial.println(DewF , 2);
+return;
+}  
 
